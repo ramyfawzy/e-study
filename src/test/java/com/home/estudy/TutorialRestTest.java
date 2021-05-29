18388b1d0a325a67ac5a50711cd3cc438d85a21d
Ramy Fawzy <eng.ramyfawzy@gmail.com>
2021-05-29 16:57:36 +0200

    Test refactor

--- src/test/java/com/home/estudy/TutorialRestTest.java
+++ src/test/java/com/home/estudy/TutorialRestTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.net.URI;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,22 +19,22 @@ import com.home.estudy.entity.Tutorial;
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TutorialRestTest {
-  
-  @Autowired
-  private TestRestTemplate restTemplate;
 
-   @Test
-   @Sql(statements = "INSERT INTO tutorial (id,description,published,title) VALUES (default,'Andra Pradesh', 1,'Elmo7n');")
-   @DisplayName("/tutorials/{id} GET tutorial exists")
-   void testFindById() {
-     
-     String title = "Elmo7n";
-     URI targetUrl= UriComponentsBuilder.fromUriString("/api/tutorials").path("/")                             
-            .path("{id}")
-            .build(129L);         
-     
-    Tutorial tutorial = this.restTemplate.getForObject(targetUrl, Tutorial.class);
-    assertEquals(tutorial.getTitle(), title);
-   }
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	private static final String TEST_FIND_BY_ID_SQL = "INSERT INTO tutorial (id,description,published,title) VALUES (default,'Andra Pradesh', 1,'Elmo7n');";
+
+	@Test
+	@Sql(statements = TEST_FIND_BY_ID_SQL)
+	@DisplayName("/tutorials/{id} GET tutorial exists")
+	void testFindById() {
+
+		String title = "Elmo7n";
+		URI targetUrl = UriComponentsBuilder.fromUriString("/api/tutorials").path("/").path("{id}").build(129L);
+
+		Tutorial tutorial = this.restTemplate.getForObject(targetUrl, Tutorial.class);
+		assertEquals(tutorial.getTitle(), title);
+	}
 
 }
